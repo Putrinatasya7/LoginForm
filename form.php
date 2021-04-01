b26943b5f8ffa6b6f8e3a18a9d49582ed09571d3
Putri Natasya <80021731+Putrinatasya7@users.noreply.github.com>
2021-04-01 15:14:27 +0700

    membuat file form.php

---
+++ form.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+ <title>Form Pendaftaran</title>
+
+</head>
+
+<body>
+
+ <form action="" method="GET">
+
+  <h1 align="center">Formulir Pendaftaran</h1>
+
+        <label>Nama Lengkap</label>
+
+        <input name="nama_lengkap" type="text">
+
+        <label>Tempat Lahir</label>
+
+        <input name="tempat_lahir" type="text">
+
+        <label>Tanggal Lahir</label>
+
+        <input name="tanggal_lahir" type="date">
+
+        <label>Jenis Kelamin</label>
+
+        <select name="jenis_kelamin">
+
+         <option value="Laki-Laki">Laki-Laki</option>
+
+         <option value="Perempuan">Perempuan</option>
+
+        </select>
+
+        <label>Alamat Lengkap</label>
+
+        <input name="alamat" type="text">
+
+        <label>E-mail</label>
+
+        <input name="email" type="text">
+
+        <label>No.HP</label>
+
+        <input name="no_hp" type="text">
+
+        <button>Daftar</button>
+
+ </form>
+
+</body>
+
+
+</html>
