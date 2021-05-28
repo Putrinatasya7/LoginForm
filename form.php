97e9822bfa3314582ea166bc378318106525f8ea
Putri Natasya <80021731+Putrinatasya7@users.noreply.github.com>
2021-05-28 11:41:07 +0700

    Update form.php

--- form.php
+++ form.php
@@ -1,82 +1,116 @@
-<?php
-require 'function.php';
+<!--
+=========================================================
+* Soft UI Dashboard - v1.0.2
+=========================================================
 
-  if( isset($_POST["submit"]) ) {
-    
-    if ( tambah($_POST) > 0) {
-      echo "
-      <script>
-        alert('data berhasil ditambahkan!');
-        document.location.href = 'form.php'
-      </script>
-      ";
-    } else {
-      echo "
-      <script>
-        alert('data gagal ditambahkan!');
-        document.location.href = 'form.php'
-      </script> 
-      ";
-    }
-  }
+* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/soft-ui-dashboard/blob/master/LICENSE.md)
 
-?>
+* Coded by Creative Tim
 
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
 <!DOCTYPE html>
-
-<html>
+<html lang="en">
 
 <head>
-
- <title>Form Pendaftaran</title>
-
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url('asset/bootstrap/img/apple-icon.png') ?>">
+  <link rel="icon" type="image/png" href="<?php echo base_url('asset/bootstrap/img/favicon.png') ?>">
+  <title>
+    Inventory System
+  </title>
+      <!-- Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="<?php echo base_url('asset/bootstrap/css/nucleo-icons.css') ?>" rel="stylesheet" />
+  <link href="<?php echo base_url('asset/bootstrap/css/nucleo-svg.css') ?>" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="<?php echo base_url('asset/bootstrap/css/nucleo-svg.css') ?>" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="<?php echo base_url('asset/bootstrap/css/soft-ui-dashboard.css?v=1.0.2') ?>" rel="stylesheet" />
 </head>
 
-<body>
+<body class="g-sidenav-show   bg-white">
+  <div class="container position-sticky z-index-sticky top-0">
+    <div class="row">
+      <div class="col-12">
+        
+      </div>
+    </div>
+  </div>
+  <section>
+    <div class="page-header section-height-75">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-5 col-lg-6 col-md-7 d-flex flex-column mx-auto">
+            <div class="card card-plain mt-8">
+              <div class="card-header pb-0 text-left bg-transparent">
+                <h3 class="font-weight-bolder text-info text-gradient">Welcome back</h3>
+                <p class="mb-0">Enter your username and password to sign in</p>
+              </div>
+              <div class="card-body">
 
- <form action="" method="GET">
+                <?php if ($this->session->flashdata('message_wrong')) : ?>
+                  <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <span class="alert-text text-white text-sm"><?= $this->session->flashdata('message_wrong'); ?></span>
+                    <button type="button" class="btn-close btn-sm" data-bs-dismiss="alert" aria-label="Close"></button>
+                  </div>
+                <?php endif; ?>
 
-  <h1 align="center">Formulir Pendaftaran</h1>
+                <form role="form text-left" action="" method="POST">
+                  <label>Username</label>
+                  <div class="mb-3">
+                    <input type="text" name="uname" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="uname-addon" value="<?= set_value("uname") ?>">
+                    <?= form_error('uname', '<small class="text-danger">', '</small>'); ?>
+                  </div>
+                  <label>Password</label>
+                  <div class="mb-3">
+                    <input type="password" name="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="password-addon">
+                    <?= form_error('password', '<small class="text-danger">', '</small>'); ?>
+                  </div>
 
-        <label>Nama Lengkap</label>
-
-        <input name="nama_lengkap" type="text">
-
-        <label>Tempat Lahir</label>
-
-        <input name="tempat_lahir" type="text">
-
-        <label>Tanggal Lahir</label>
-
-        <input name="tanggal_lahir" type="date">
-
-        <label>Jenis Kelamin</label>
-
-        <select name="jenis_kelamin">
-
-         <option value="Laki-Laki">Laki-Laki</option>
-
-         <option value="Perempuan">Perempuan</option>
-
-        </select>
-
-        <label>Alamat Lengkap</label>
-
-        <input name="alamat" type="text">
-
-        <label>E-mail</label>
-
-        <input name="email" type="text">
-
-        <label>No.HP</label>
-
-        <input name="no_hp" type="text">
-
-        <button>Daftar</button>
-
- </form>
-
+                  <div class="text-center">
+                    <button type="submit" class="btn bg-gradient-info w-100 mt-4 mb-0">Sign in</button>
+                  </div>
+                </form>
+              </div>
+              <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="oblique position-absolute top-0 h-100 d-md-block d-none me-n8">
+              <div class="oblique-image bg-cover position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6" style="background-image:url('asset/bootstrap/img/curved-images/curved6.jpg')"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--   Core JS Files   -->
+  <script src="<?= base_url(); ?>/asset/bootstrap/js/core/popper.min.js"></script>
+  <script src="<?= base_url(); ?>/asset/bootstrap/js/core/bootstrap.min.js"></script>
+  <script src="<?= base_url(); ?>/asset/bootstrap/js/plugins/smooth-scrollbar.min.js"></script>
+  <script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="<?= base_url(); ?>/asset/bootstrap/js/soft-ui-dashboard.min.js?v=1.0.2"></script>
 </body>
-
 
 </html>
